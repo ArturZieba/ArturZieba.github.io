90e5770245a79f40ebf1e04263c496e619623b60
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-03-02 15:49:17 +0100

    Add console print functionality to buttons on Selenium Test page
    - Move <script> to <head> with defer argument in all .html files to make the scripts load after the rest of the page.
    - Add a function for printing to Console.
    - Assign printing function to test buttons on the Selenium Test page and confirm that they work.

--- seleniumtest.js
+++ seleniumtest.js
@@ -1,1 +1,10 @@
+function printToConsole(value)
+{
+    console.log(value)
+}
+
 console.log("Hello, seleniumtest!")
+
+document.getElementById('st-b0').onclick = () => printToConsole("Button 1");
+document.getElementById('st-b1').onclick = () => printToConsole("Button 2");
+document.getElementById('st-b2').onclick = () => printToConsole("Button 3");
