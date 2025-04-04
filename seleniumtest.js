6141c788cf122d9c6b97820d398cb04c2847b720
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-04-04 18:56:38 +0200

    Fix Canvas on Selenium Test page
    - Adjust seleniumtest.html and .js so that canvas element is properly drawn.

--- seleniumtest.js
+++ seleniumtest.js
@@ -9,12 +9,14 @@ document.getElementById('st-b0').onclick = () => printToConsole("Button 1");
 document.getElementById('st-b1').onclick = () => printToConsole("Button 2");
 document.getElementById('st-b2').onclick = () => printToConsole("Button 3");
 
-let st-canvas0 = document.getElementById("st-canvas0");
-let st-canvas0-context = st-canvas0.getContext("2d");
-st-canvas0-context.globalAlpha = 0.3;
-st-canvas0-context.fillStyle("red");
-st-canvas0-context.fillRect(0, 50, 50, 50);
-st-canvas0-context.fillStyle("green");
-st-canvas0-context.fillRect(25, 75, 75, 50);
-st-canvas0-context.fillStyle("blue");
-st-canvas0-context.fillRect(25, 25, 25, 50);
+// Draw on Selenium Test page canvas element (st-canvas0)
+let stCanvas0 = document.getElementById("st-canvas0");
+let stCanvas0Context = stCanvas0.getContext("2d");
+stCanvas0Context.globalAlpha = 0.3;
+
+stCanvas0Context.fillStyle = "red";
+stCanvas0Context.fillRect(0, 50, 50, 50);
+stCanvas0Context.fillStyle = "green";
+stCanvas0Context.fillRect(25, 75, 75, 50);
+stCanvas0Context.fillStyle = "blue";
+stCanvas0Context.fillRect(25, 25, 25, 50);
